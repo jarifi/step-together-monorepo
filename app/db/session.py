60e1959e84956ffc4deb3a8bbcacf2aadf094075
MaxxG99 <166509773+MaxxG99@users.noreply.github.com>
2025-07-01 10:03:30 +0200

    databasename changed

--- app/db/session.py
+++ app/db/session.py
@@ -2,7 +2,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root@localhost:3306/step_together"
+SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root@localhost:3306/step_together_api"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
--- app/db/step-together-api.sql
+++ app/db/step-together-api.sql
@@ -15,12 +15,12 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 
--- Exportiere Datenbank-Struktur für step_together
-DROP DATABASE IF EXISTS `step_together`;
-CREATE DATABASE IF NOT EXISTS `step_together` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci */;
-USE `step_together`;
+-- Exportiere Datenbank-Struktur für step_together_api
+DROP DATABASE IF EXISTS `step_together_api`;
+CREATE DATABASE IF NOT EXISTS `step_together_api` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci */;
+USE `step_together_api`;
 
--- Exportiere Struktur von Tabelle step_together.challenges
+-- Exportiere Struktur von Tabelle step_together_api.challenges
 DROP TABLE IF EXISTS `challenges`;
 CREATE TABLE IF NOT EXISTS `challenges` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -37,14 +37,14 @@ CREATE TABLE IF NOT EXISTS `challenges` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
--- Exportiere Daten aus Tabelle step_together.challenges: ~3 rows (ungefähr)
+-- Exportiere Daten aus Tabelle step_together_api.challenges: ~3 rows (ungefähr)
 DELETE FROM `challenges`;
 INSERT INTO `challenges` (`id`, `name`, `start_ort`, `ziel_ort`, `streckenlaenge`, `start_datum`, `end_datum`, `ersteller_id`, `team_id`, `created_at`, `updated_at`) VALUES
 	(1, 'Spring Challenge', 'Berlin', 'Hamburg', 300, '2025-05-01 00:00:00.000', '2025-06-01 00:00:00.000', 1, 1, '2025-05-14 11:44:47.646', '2025-05-14 11:44:47.646'),
 	(2, 'Spring Challenge', 'Berlin', 'Hamburg', 300, '2025-05-01 00:00:00.000', '2025-06-01 00:00:00.000', 1, 2, '2025-05-14 13:42:50.754', '2025-05-14 13:42:50.754'),
 	(3, 'Spring Challenge', 'Berlin', 'Hamburg', 300, '2025-05-01 00:00:00.000', '2025-06-01 00:00:00.000', 1, 3, '2025-05-20 08:23:39.224', '2025-05-20 08:23:39.224');
 
--- Exportiere Struktur von Tabelle step_together.challenge_progress
+-- Exportiere Struktur von Tabelle step_together_api.challenge_progress
 DROP TABLE IF EXISTS `challenge_progress`;
 CREATE TABLE IF NOT EXISTS `challenge_progress` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -56,7 +56,7 @@ CREATE TABLE IF NOT EXISTS `challenge_progress` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
--- Exportiere Daten aus Tabelle step_together.challenge_progress: ~6 rows (ungefähr)
+-- Exportiere Daten aus Tabelle step_together_api.challenge_progress: ~6 rows (ungefähr)
 DELETE FROM `challenge_progress`;
 INSERT INTO `challenge_progress` (`id`, `user_id`, `challenge_id`, `gelaufene_strecke`, `insgesamte_schritte`, `updated_at`) VALUES
 	(1, 1, 1, 7, 10000, '2025-05-14 11:44:47.654'),
@@ -66,7 +66,7 @@ INSERT INTO `challenge_progress` (`id`, `user_id`, `challenge_id`, `gelaufene_st
 	(5, 1, 3, 7, 10000, '2025-05-20 08:23:39.232'),
 	(6, 2, 3, 6, 8000, '2025-05-20 08:23:39.232');
 
--- Exportiere Struktur von Tabelle step_together.challenge_progresses
+-- Exportiere Struktur von Tabelle step_together_api.challenge_progresses
 DROP TABLE IF EXISTS `challenge_progresses`;
 CREATE TABLE IF NOT EXISTS `challenge_progresses` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -83,10 +83,10 @@ CREATE TABLE IF NOT EXISTS `challenge_progresses` (
   CONSTRAINT `challenge_progresses_ibfk_2` FOREIGN KEY (`challenge_id`) REFERENCES `challenges` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- Exportiere Daten aus Tabelle step_together.challenge_progresses: ~0 rows (ungefähr)
+-- Exportiere Daten aus Tabelle step_together_api.challenge_progresses: ~0 rows (ungefähr)
 DELETE FROM `challenge_progresses`;
 
--- Exportiere Struktur von Tabelle step_together.schritt_logs
+-- Exportiere Struktur von Tabelle step_together_api.schritt_logs
 DROP TABLE IF EXISTS `schritt_logs`;
 CREATE TABLE IF NOT EXISTS `schritt_logs` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -98,7 +98,7 @@ CREATE TABLE IF NOT EXISTS `schritt_logs` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
--- Exportiere Daten aus Tabelle step_together.schritt_logs: ~6 rows (ungefähr)
+-- Exportiere Daten aus Tabelle step_together_api.schritt_logs: ~6 rows (ungefähr)
 DELETE FROM `schritt_logs`;
 INSERT INTO `schritt_logs` (`id`, `user_id`, `challenge_id`, `team_id`, `datum`, `anzahl_schritte`) VALUES
 	(1, 1, 1, 1, '2025-05-03 00:00:00.000', 10000),
@@ -108,7 +108,7 @@ INSERT INTO `schritt_logs` (`id`, `user_id`, `challenge_id`, `team_id`, `datum`,
 	(5, 1, 3, 3, '2025-05-03 00:00:00.000', 10000),
 	(6, 2, 3, 3, '2025-05-03 00:00:00.000', 8000);
 
--- Exportiere Struktur von Tabelle step_together.teams
+-- Exportiere Struktur von Tabelle step_together_api.teams
 DROP TABLE IF EXISTS `teams`;
 CREATE TABLE IF NOT EXISTS `teams` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -119,14 +119,14 @@ CREATE TABLE IF NOT EXISTS `teams` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
--- Exportiere Daten aus Tabelle step_together.teams: ~3 rows (ungefähr)
+-- Exportiere Daten aus Tabelle step_together_api.teams: ~3 rows (ungefähr)
 DELETE FROM `teams`;
 INSERT INTO `teams` (`id`, `name`, `creator_id`, `created_at`, `updated_at`) VALUES
 	(1, 'Team Alpha', 1, '2025-05-14 11:44:47.637', '2025-05-14 11:44:47.637'),
 	(2, 'Team Alpha', 1, '2025-05-14 13:42:50.744', '2025-05-14 13:42:50.744'),
 	(3, 'Team Alpha', 1, '2025-05-20 08:23:39.215', '2025-05-20 08:23:39.215');
 
--- Exportiere Struktur von Tabelle step_together.team_members
+-- Exportiere Struktur von Tabelle step_together_api.team_members
 DROP TABLE IF EXISTS `team_members`;
 CREATE TABLE IF NOT EXISTS `team_members` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -136,7 +136,7 @@ CREATE TABLE IF NOT EXISTS `team_members` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
--- Exportiere Daten aus Tabelle step_together.team_members: ~6 rows (ungefähr)
+-- Exportiere Daten aus Tabelle step_together_api.team_members: ~6 rows (ungefähr)
 DELETE FROM `team_members`;
 INSERT INTO `team_members` (`id`, `user_id`, `team_id`, `beitrittsdatum`) VALUES
 	(1, 1, 1, '2025-05-01 00:00:00.000'),
@@ -146,7 +146,7 @@ INSERT INTO `team_members` (`id`, `user_id`, `team_id`, `beitrittsdatum`) VALUES
 	(5, 1, 3, '2025-05-01 00:00:00.000'),
 	(6, 2, 3, '2025-05-02 00:00:00.000');
 
--- Exportiere Struktur von Tabelle step_together.users
+-- Exportiere Struktur von Tabelle step_together_api.users
 DROP TABLE IF EXISTS `users`;
 CREATE TABLE IF NOT EXISTS `users` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -166,7 +166,7 @@ CREATE TABLE IF NOT EXISTS `users` (
   UNIQUE KEY `users_email_key` (`email`)
 ) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
--- Exportiere Daten aus Tabelle step_together.users: ~6 rows (ungefähr)
+-- Exportiere Daten aus Tabelle step_together_api.users: ~6 rows (ungefähr)
 DELETE FROM `users`;
 INSERT INTO `users` (`id`, `name`, `email`, `hashed_password`, `schrittlaenge`, `is_active`, `is_verified`, `verification_token`, `password_reset_token`, `failed_login_attempts`, `locked_until`, `created_at`, `updated_at`) VALUES
 	(1, 'Test User', 'alice@example.com', '$2b$12$O2zu3sjsx3k8CtygosOyT.ynRP5ctnJuNyi3AECxhf/qaRaZAgbXS', 0.75, 1, 0, NULL, NULL, 0, NULL, '2025-06-26 11:49:36.046', '2025-06-26 13:49:57.175'),
