636b40157c210bbd2f3214cb76932473d7f49609
jetton77 <jetonbfi@gmail.com>
2025-07-02 08:21:18 +0200

    config file added

--- app/db/session.py
+++ app/db/session.py
@@ -1,12 +1,13 @@
-# db/session.py
+# app/db/session.py
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from app.core.config import settings  # <-- Import from your config
 
-SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root@localhost:3306/step_together_api"
-
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+# Use the URL from the .env or config.py
+engine = create_engine(settings.SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-# This is the function you're missing:
+
 def get_db():
     db = SessionLocal()
     try:
