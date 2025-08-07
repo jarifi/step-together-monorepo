17c47352188eab574cc938794b93e46eaab5d799
jetton77 <jetonbfi@gmail.com>
2025-08-07 10:37:28 +0200

    list users

--- services/userService.js
+++ services/userService.js
@@ -1,11 +1,25 @@
-import Constants from 'expo-constants';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
-const { BASE_URL } = Constants.expoConfig.extra;
+const BASE_URL = 'http://127.0.0.1:3000/api/v1';
 
 export const getUsers = async () => {
   try {
-    const res = await fetch(`${BASE_URL}/users`);
+    const token = await AsyncStorage.getItem('userToken');
+
+    const res = await fetch(`${BASE_URL}/users/`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${token}`,
+      },
+    });
+
     const users = await res.json();
+
+    if (!res.ok) {
+      throw new Error(users.message || 'Failed to fetch users');
+    }
+
     return users.map((u, i) => ({
       ...u,
       avatar: `https://i.pravatar.cc/150?img=${i + 1}`,
