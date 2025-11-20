7fb53aabae621ede33a9b388fe26266dbdcebe51
Sara <sara.kutschi@hotmail.com>
2025-11-20 12:42:19 +0100

    small fix💕

--- app/styles/dashboardStyles.ts
+++ app/styles/dashboardStyles.ts
@@ -19,7 +19,7 @@ const styles = StyleSheet.create({
         //paddingTop: 100,
         backgroundColor: 'transparent',
         padding: 0,
-        marginTop: 60
+        marginTop: 100
     },
 
     // ---------- expired challenge warning banner ----------
